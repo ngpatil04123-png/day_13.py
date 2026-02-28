139838b24a639a0b04e344b996f88d2e4307b070
Navollas patil <ngpatil04123@gmail.com>
2026-02-28 23:53:17 +0530

    Restore palindrome check functionality

--- day_13.py
+++ day_13.py
@@ -1,17 +1,17 @@
-# print("Never give up !!")
-# num=int(input("Enter the number to check wether palindrome or not : "))
-# org=num
-# reverse=0
-# while num > 0:
-#     last = num % 10
-#     reverse=reverse*10+last
-#     num=num//10
-# if org==reverse:
-#     print("its a palindrome number : ")
-# else:
-#     print("its not an palindrome number ")
+print("Never give up !!")
+num=int(input("Enter the number to check wether palindrome or not : "))
+org=num
+reverse=0
+while num > 0:
+    last = num % 10
+    reverse=reverse*10+last
+    num=num//10
+if org==reverse:
+    print("its a palindrome number : ")
+else:
+    print("its not an palindrome number ")
 
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 name=str(input("Enter the string to check wether palindrome or not : "))
@@ -30,10 +30,11 @@ if cln==rev:
 else:
     print("its not a palindrome string")
 
-# if name==name[::-1]:
-#     print(name)
-#     print("its a palindrome  : ")
-# else:
-#     print("its not an palindrome  ")
+if name==name[::-1]:
+    print(name)
+    print("its a palindrome  : ")
+else:
+    print("its not an palindrome  ")
+
 
 
